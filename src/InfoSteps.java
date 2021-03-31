124d066ace9671429310cb5aded2becf721f633a
natascha <natalia.andrienko@iais.fraunhofer.de>
2021-03-31 16:43:55 +0200

    fixed order of selected steps to be presented in InfoCanvasAll

--- src/InfoSteps.java
+++ src/InfoSteps.java
@@ -2,9 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.TreeSet;
 
