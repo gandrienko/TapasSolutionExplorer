5a415ac290472b1ae8dca428e148c63182d497b9
natascha <natalia.andrienko@iais.fraunhofer.de>
2021-04-20 11:21:41 +0200

    comparison of multiple scenarios

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
   public static void main(String[] args) {
     DataKeeper dk=null;
+    if (args.length>=3 && args[2].indexOf('=')>0) {
+      String s[]=new String[args.length-2];
+      for (int i=0; i<s.length; i++)
+        s[i]=args[i+2];
+      dk=new DataKeeper(args[0],args[1],s); // fnCapacities,fnDecisions,fnFlightPlans);
+    }
+    else
     if (args.length==2)
       dk=new DataKeeper(args[0],args[1]);
     else
--- src/TapasSolutionExplorer/Data/DataKeeper.java
+++ src/TapasSolutionExplorer/Data/DataKeeper.java
@@ -574,4 +574,26 @@ public class DataKeeper {
     calcFeaturesOfSteps();
   }
 
+  public DataKeeper (String fnCapacities, String fnFlightPlans, String fnSolutions[]) {
+    capacities=TapasDataReader.Readers.readCapacities(fnCapacities);
+    TreeSet<Integer> steps=new TreeSet();
+    steps.add(new Integer(-1)); // step -1 represents the baseline solution
+    Nsteps=1+fnSolutions.length;
+    stepLabels=new String[Nsteps];
+    stepLabels[0]="baseline";
+    records=TapasDataReader.Readers.readFlightPlans(fnFlightPlans, -1,null);
+    for (int i=0; i<fnSolutions.length; i++) {
+      int pos=fnSolutions[i].indexOf('=');
+      stepLabels[1+i]=fnSolutions[i].substring(0,pos);
+      String fn=fnSolutions[i].substring(pos+1);
+      Hashtable<String,Vector<Record>> extraRecords=TapasDataReader.Readers.readSolutionAsStep(fn,i+1);
+      for (String str:extraRecords.keySet())
+        records.put(str,extraRecords.get(str));
+    }
+    for (String s:records.keySet())
+      sectors.add(s.substring(0,s.indexOf("_")));
+    aggregateAll();
+    calcFeaturesOfSteps();
+  }
+
 }
